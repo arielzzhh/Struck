a74f4cfdc96cfd1ddf06f09a98c1fa62a9674969
arielzzhh <121157336+arielzzhh@users.noreply.github.com>
2024-12-18 17:02:44 +0200

    Add files via upload

---
+++ Item/Upper.java
@@ -0,0 +1,14 @@
+package Item ;
+import java.util.ArrayList;
+
+public class Upper extends Clothes {
+ //
+
+    
+public Upper (String name,int physical,byte[][] Perks ){
+    super(name,physical,Perks);
+    }
+    
+    
+    
+}
