e631a4b310f64839523cd3c503f6293937511a7e
arielzzhh <121157336+arielzzhh@users.noreply.github.com>
2024-12-29 16:54:30 +0200

    Add files via upload

---
+++ Item/ShortSword.java
@@ -0,0 +1,26 @@
+package Item ;
+import java.util.ArrayList;
+
+public class ShortSword extends Hand {
+    
+ 
+    
+    
+
+    
+public ShortSword (String name,int physical,byte[][] Perks ){
+    super(name,physical,Perks);
+    }
+    
+    
+    
+    
+    
+    public getShortSword(){
+    
+    
+    }
+    
+    
+    
+}
